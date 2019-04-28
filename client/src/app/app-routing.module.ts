567817c1a8de30ce7e222d787e43ed1d37ae16aa
Estêvão Fáy <eff.fay@gmail.com>
2019-04-28 17:53:36 -0300

    Organize App's Pages inside pages folder

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,18 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'pessoas-cadastradas', loadChildren: './pessoas-cadastradas/pessoas-cadastradas.module#PessoasCadastradasPageModule' },
-  { path: 'home-administrador', loadChildren: './home-administrador/home-administrador.module#HomeAdministradorPageModule' },
-  { path: 'todas-as-turmas', loadChildren: './todas-as-turmas/todas-as-turmas.module#TodasAsTurmasPageModule' },
-  { path: 'home-student', loadChildren: './home-student/home-student.module#HomeStudentPageModule' },
-  { path: 'selecionar-turma-para-chamada', loadChildren: './selecionar-turma-para-chamada/selecionar-turma-para-chamada.module#SelecionarTurmaParaChamadaPageModule' },
-  { path: 'minha-turma', loadChildren: './minha-turma/minha-turma.module#MinhaTurmaPageModule' },
-  { path: 'faltas', loadChildren: './faltas/faltas.module#FaltasPageModule' },  { path: 'transferencia-student', loadChildren: './transferencia-student/transferencia-student.module#TransferenciaStudentPageModule' },
-  { path: 'cadastrar-novo-curso-student', loadChildren: './cadastrar-novo-curso-student/cadastrar-novo-curso-student.module#CadastrarNovoCursoStudentPageModule' },
-  { path: 'chamada', loadChildren: './chamada/chamada.module#ChamadaPageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'pessoas-cadastradas', loadChildren: './pages/pessoas-cadastradas/pessoas-cadastradas.module#PessoasCadastradasPageModule' },
+  { path: 'home-administrador', loadChildren: './pages/home-administrador/home-administrador.module#HomeAdministradorPageModule' },
+  { path: 'todas-as-turmas', loadChildren: './pages/todas-as-turmas/todas-as-turmas.module#TodasAsTurmasPageModule' },
+  { path: 'home-student', loadChildren: './pages/home-student/home-student.module#HomeStudentPageModule' },
+  { path: 'selecionar-turma-para-chamada', loadChildren: './pages/selecionar-turma-para-chamada/selecionar-turma-para-chamada.module#SelecionarTurmaParaChamadaPageModule' },
+  { path: 'minha-turma', loadChildren: './pages/minha-turma/minha-turma.module#MinhaTurmaPageModule' },
+  { path: 'faltas', loadChildren: './pages/faltas/faltas.module#FaltasPageModule' },
+  { path: 'transferencia-student', loadChildren: './pages/transferencia-student/transferencia-student.module#TransferenciaStudentPageModule' },
+  { path: 'cadastrar-novo-curso-student', loadChildren: './pages/cadastrar-novo-curso-student/cadastrar-novo-curso-student.module#CadastrarNovoCursoStudentPageModule' },
+  { path: 'chamada', loadChildren: './pages/chamada/chamada.module#ChamadaPageModule' },
 
 
 

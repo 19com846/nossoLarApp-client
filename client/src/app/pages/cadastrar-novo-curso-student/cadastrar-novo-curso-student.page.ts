9491a5339676980db62b299e6d17e09cb5177e18
Estêvão Fáy <eff.fay@gmail.com>
2019-05-02 19:27:03 -0300

    Added Static API to cadastrar em novo curso page
    Set working Callback with Static API
    Fix some UI issues to display proper data

--- client/src/app/pages/cadastrar-novo-curso-student/cadastrar-novo-curso-student.page.ts
+++ client/src/app/pages/cadastrar-novo-curso-student/cadastrar-novo-curso-student.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-import { Router } from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router'
+import { StaticAPIService } from 'src/app/services/static-api.service';
 
 @Component({
   selector: 'app-cadastrar-novo-curso-student',
@@ -9,107 +10,47 @@ import { Router } from '@angular/router'
 })
 export class CadastrarNovoCursoStudentPage implements OnInit {
 
-  public turma: Array<Object> =[];
+  public turmas: Array<Object> = [];
 
-  constructor(public alertController: AlertController, private router: Router) { 
-    this.turma = [
-      {
-        "id": "123",
-        "name": "Curso em X",
-        "typeOfClass": "Turma Livre",
-        "location": "Sala 7/8",
-        "weekday": "Segunda-feira",
-        "startingWeek": "20",
-        "endingWeek": "46",
-        "startTime": "22:00",
-        "status": "on",
-        "endTime": "23:30",
-        "collaborators": [
-          {
-            "id": "123124",
-            "name": "Fulano de Tal",
-            "email": "fulano.tal@gmail.com",
-            "phone": "19999091120"
-          },
-          {
-            "id": "897987",
-            "name": "Sicrano",
-            "email": "sicrano.silva@hotmail.com",
-            "phone": "null"
-          }
-        ]
-      },
-      {
-        "id": "456",
-        "name": "Constelações",
-        "typeOfClass": "Palestra",
-        "location": "Sala SSP",
-        "weekday": "Sabado",
-        "startingWeek": "20",
-        "endingWeek": "46",
-        "status": "on",
-        "startTime": "13:00",
-        "endTime": "15:30",
-        "collaborators": [
-          {
-            "id": "123124",
-            "name": "Joaozin",
-            "email": "fulano.tal@gmail.com",
-            "phone": "19999091120"
-          },
-          {
-            "id": "897987",
-            "name": "Sicrano",
-            "email": "sicrano.silva@hotmail.com",
-            "phone": "null"
-          }
-        ]
-      }, {
-        "id": "789",
-        "name": "Palestra tema X",
-        "typeOfClass": "Turma Livre",
-        "location": "Sala SSP",
-        "weekday": "Quarta-feira",
-        "startingWeek": "20",
-        "endingWeek": "46",
-        "status": "waiting",
-        "startTime": "20:00",
-        "endTime": "21:300",
-        "collaborators": [
-          {
-            "id": "123124",
-            "name": "Ronaldo",
-            "email": "fulano.tal@gmail.com",
-            "phone": "19999091120"
-          }
-        ]
-      },
-     ]
-  }
+  constructor(public alertController: AlertController,
+              private router: Router,
+              private route: ActivatedRoute,
+              private api: StaticAPIService) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params.id;
+    this.getOpenTurmas(id);
   }
 
-  cardClicked(turmas) {
+  getOpenTurmas(id: number) {
+    this.api.getOpenTurmas(id).subscribe((data: Array<object>) => {
+      this.turmas = data;
+      // console.log(data);
+    });
+  }
+
+  cardClicked(turma) {
     this.router.navigate(['home-student']);
   }
-  async presentAlertConfirm(turmas) {
+
+  async presentAlertConfirm(turma) {
     const alert = await this.alertController.create({
-      header: 'Atenção!',
-      message: "Você deseja mesmo se candidatar para o curso: "+turmas.name+" ?",
+      header: 'Confirmar!',
+      message: 'Informações da Turma: \n' + turma.course + ' - ' + turma.title + ' - ' + turma.time  +
+                '\n Você deseja mesmo se candidatar para essa turma?',
       buttons: [
         {
           text: 'Não',
           role: 'cancel',
           cssClass: 'secondary',
-          handler: (blah) => {
-            console.log('Confirm Cancel: blah');
+          handler: () => {
+            //console.log('Confirm Cancel: ');
           }
         }, {
           text: 'SIM !',
           handler: () => {
             console.log('Confirm Okay');
-            this.cardClicked(turmas);
+            this.cardClicked(turma);
           }
         }
       ]

df13e89579e8b4c28bdf3b37b12b3fa4f3dae932
Estêvão Fáy <eff.fay@gmail.com>
2019-05-11 14:14:24 -0300

    Create main API endpoints and logic
    Add Turma (turma-list) PATH
    Add Turma (turma-detail) PATH
    Add Students (student-list) PATH
    Add Student (student-detail) PATH
    Add Student's Turmas (student-turma-list) PATH
    
    Add TurmaSerializer
    
    Add TurmasDoAlunoApi

--- server/appbackend/serializers.py
+++ server/appbackend/serializers.py
@@ -1,14 +1,43 @@
 from rest_framework import serializers
 
-from .models import Turma, Person
+from .models import Turma, Person, Course, Enrolled
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ('id', 'name', 'phone', 'permission')
+
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
-        fields = ('name', 'phone')
+        fields = ('id', 'name', 'phone', 'permission')
+
+
+class CollaboratorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ('id', 'name', 'phone', 'permission')
+
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrolled
+        fields = '__all__'
+        # fields = ('id', 'active', 'status', 'finalGrade', 'graduated')
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
 
 class TurmaSerializer(serializers.ModelSerializer):
-    teacher = TeacherSerializer()
+    teacher = serializers.ReadOnlyField(source='teacher.name')
+    collaborators = CollaboratorSerializer(many=True, read_only=True)
+    enrollments = EnrollmentSerializer(many=True, read_only=True)
+    course = CourseSerializer()
     class Meta:
         model = Turma
         fields = '__all__'
--- server/appbackend/urls.py
+++ server/appbackend/urls.py
@@ -1,7 +1,17 @@
-from django.conf.urls import url
-from .api import TurmaApi, TurmasDoAlunoApi
+from django.conf.urls import *
+from django.urls import path
+from rest_framework import routers
+from .api import TurmaApi, TurmasDoAlunoApi, CourseApi, TurmaDetailApi, StudentApi, StudentDetailApi
 
 urlpatterns = [
-    url('turmas', TurmaApi.as_view()),
-    url('student', TurmasDoAlunoApi.as_view())
+    path('turmas/', TurmaApi.as_view(), name='turma-list'),
+    path('turmas/<int:pk>', TurmaDetailApi.as_view(), name='turma-detail'),
+    path('students/', StudentApi.as_view(), name='student-list'),
+    path('students/<int:pk>/', StudentDetailApi.as_view(), name='student-detail'),
+    path('students/<int:pk>/turmas', TurmasDoAlunoApi.as_view(), name='student-turmas-list')
+    # url('courses', CourseApi.as_view()),
+    # url(r'^', include(router.urls))
+
 ]
+
+

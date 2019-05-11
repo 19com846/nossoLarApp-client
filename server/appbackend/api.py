02b327a0c244acf018f374971df79f44f43be399
Estêvão Fáy <eff.fay@gmail.com>
2019-05-11 14:14:24 -0300

    Create main API endpoints and logic
    Add Turma (turma-list) PATH
    Add Turma (turma-detail) PATH
    Add Students (student-list) PATH
    Add Student (student-detail) PATH
    Add Student's Turmas (student-turma-list) PATH
    
    Add TurmaSerializer
    
    Add TurmasDoAlunoApi

--- server/appbackend/api.py
+++ server/appbackend/api.py
@@ -1,21 +1,49 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 
-from .serializers import TurmaSerializer
-from .models import Turma
+from .serializers import TurmaSerializer, CourseSerializer, StudentSerializer
+from .models import Turma, Course, Person
 
-class TurmaApi(ListAPIView):
+
+class StudentApi(generics.ListAPIView):
+    serializer_class = StudentSerializer
+    def get_queryset(self):
+        list_of_students = []
+        for s in Person.objects.all():
+            if s.permission == 0:
+                list_of_students.append(s)
+        return list_of_students
+
+
+class StudentDetailApi(generics.RetrieveAPIView):
+    serializer_class = StudentSerializer
+    queryset = Person.objects.all()
+
+
+class CourseApi(generics.ListAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class TurmaApi(generics.ListAPIView):
     queryset = Turma.objects.all()
     serializer_class = TurmaSerializer
 
-class TurmasDoAlunoApi(ListAPIView):
+
+class TurmaDetailApi(generics.RetrieveAPIView):
+    queryset = Turma.objects.all()
+    serializer_class = TurmaSerializer
+
+
+class TurmasDoAlunoApi(generics.ListAPIView):
     serializer_class = TurmaSerializer
     def get_queryset(self):
-        studentID = int(self.request.query_params.get('studentID', '0'))
-        allTurmas = Turma.objects.all()
-        listOfTurmas = []
-        for t in allTurmas:
+        student_id = self.kwargs['pk']
+        print("student_id: {}".format(student_id))
+        all_turmas = Turma.objects.all()
+        list_of_turmas = []
+        for t in all_turmas:
             for e in t.enrollments.all():
-                if e.student.id == studentID:
+                if e.student.id == student_id:
                     print("show")
-                    listOfTurmas.append(t)
-        return listOfTurmas
+                    list_of_turmas.append(t)
+        return list_of_turmas

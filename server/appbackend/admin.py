a1d2386244b6d661d90c03e21a73e869264e7252
Henrique Oliveira <henrique.verrengia@gmail.com>
2019-05-13 20:32:07 -0300

    Refactored admin to standardize language and added TransferRequest register

--- server/appbackend/admin.py
+++ server/appbackend/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Course, Person, Enrolled, Turma, Lesson, Presence
+from .models import Course, Person, Enrollment, ClassGroup, Lesson, Attendance, TransferRequest
 
 admin.site.register(Course)
 admin.site.register(Person)
-admin.site.register(Enrolled)
-admin.site.register(Turma)
+admin.site.register(Enrollment)
+admin.site.register(ClassGroup)
 admin.site.register(Lesson)
-admin.site.register(Presence)
+admin.site.register(Attendance)
+admin.site.register(TransferRequest)

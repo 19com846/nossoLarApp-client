d1c0535413cefff63b37129fd259bcda1cc729f3
Henrique Oliveira <henrique.verrengia@gmail.com>
2019-05-13 20:30:50 -0300

    Added TransferRequest and ConfirmTransfer endpoints.

--- server/appbackend/urls.py
+++ server/appbackend/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('turmas/<int:pk>', ClassGroupDetailApi.as_view(), name='turma-detail'),
     path('students/', StudentApi.as_view(), name='student-list'),
     path('students/<int:pk>/', StudentDetailApi.as_view(), name='student-detail'),
-    path('students/<int:pk>/class-groups', StudentClassGroupsApi.as_view(), name='student-turmas-list')
+    path('students/<int:pk>/class-groups', StudentClassGroupsApi.as_view(), name='student-turmas-list'),
+    path('students/<int:pk>/class-groups/transfer', RequestTransferApi.as_view(), name='request-student-transfer'),
+    path('transfer-requests/<int:pk>/confirm', ConfirmTransferApi.as_view(), name='confirm-student-transfer')
     # url('courses', CourseApi.as_view()),
     # url(r'^', include(router.urls))
 

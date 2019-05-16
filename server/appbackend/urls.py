b433190e98904ccee5eeb7d95d2036d85bdf4a7f
Henrique Oliveira <henrique.verrengia@gmail.com>
2019-05-15 21:51:04 -0300

    Fixed urls.

--- server/appbackend/urls.py
+++ server/appbackend/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from rest_framework import routers
 from .api import *
 urlpatterns = [
-    path('turmas/', ClassGroupApi.as_view(), name='turma-list'),
-    path('turmas/<int:pk>', ClassGroupDetailApi.as_view(), name='turma-detail'),
+    path('class-groups/', ClassGroupApi.as_view(), name='turma-list'),
+    path('class-groups/<int:pk>', ClassGroupDetailApi.as_view(), name='turma-detail'),
     path('students/', StudentApi.as_view(), name='student-list'),
     path('students/<int:pk>/', StudentDetailApi.as_view(), name='student-detail'),
     path('students/<int:pk>/class-groups', StudentClassGroupsApi.as_view(), name='student-turmas-list'),

88d5525bec474553e3e33001d9defa9051a5ecc4
Henrique Oliveira <henrique.verrengia@gmail.com>
2019-05-17 19:39:52 -0300

    Added everything.

--- server/appbackend/admin.py
+++ server/appbackend/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
 
+from appbackend.forms import PersonCreationForm, PersonChangeForm
 from .models import Course, Person, Enrollment, ClassGroup, Lesson, Attendance, TransferRequest
 
+
+class PersonAdmin(UserAdmin):
+    add_form = PersonCreationForm
+    form = PersonChangeForm
+    model = Person
+    list_display = ['email', 'phone', 'first_name', 'last_name', 'password', ]
+
+
 admin.site.register(Course)
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Enrollment)
 admin.site.register(ClassGroup)
 admin.site.register(Lesson)
--- server/appbackend/api.py
+++ server/appbackend/api.py
@@ -1,19 +1,25 @@
-from rest_framework import generics, status
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import Group
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
+from rest_framework_jwt.settings import api_settings
 
 from .serializers import *
 from .models import ClassGroup, Course, Person
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 
 class StudentApi(generics.ListAPIView):
     serializer_class = StudentSerializer
 
     def get_queryset(self):
-        list_of_students = []
-        for s in Person.objects.all():
-            if s.role == Role.STUDENT:
-                list_of_students.append(s)
-        return list_of_students
+        students = list(Person.objects.filter(groups__name='Students').values())
+        return Response(
+            data=StudentSerializer(students, many=True).data,
+            status=status.HTTP_200_OK)
 
 
 class StudentDetailApi(generics.RetrieveAPIView):
@@ -38,6 +44,7 @@ class ClassGroupDetailApi(generics.RetrieveAPIView):
 
 class StudentClassGroupsApi(generics.ListAPIView):
     serializer_class = ClassGroupSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
         student_id = self.kwargs['pk']
@@ -90,5 +97,74 @@ class CreateLessonApi(generics.CreateAPIView):
         )
         return Response(
             data=LessonSerializer(lesson).data,
+            status=status.HTTP_201_CREATED
+        )
+
+
+class CreateAttendanceApi(generics.CreateAPIView):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+
+    def post(self, request, *args, **kwargs):
+        lesson = Lesson.objects.filter(pk=kwargs["pk"])[:1].get()
+        students = list(Person.objects.filter(pk__in=request.data["student_ids"]).all())
+        attendances = list()
+        for student in students:
+            attendance = Attendance.objects.create(
+                lesson=lesson,
+                student=student
+            )
+            attendances.append(attendance)
+        return Response(
+            data=AttendanceSerializer(attendances, many=True).data,
+            status=status.HTTP_201_CREATED
+        )
+
+
+class LoginApi(generics.CreateAPIView):
+    permission_classes = (permissions.AllowAny,)
+
+    queryset = Person.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get("email", "")
+        password = request.data.get("password", "")
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            serializer = TokenSerializer(data={
+                "token": jwt_encode_handler(
+                    jwt_payload_handler(user)
+                )})
+            serializer.is_valid()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+class RegisterApi(generics.CreateAPIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username", "")
+        email = request.data.get("email", "")
+        password = make_password(request.data.get("password", ""))
+        phone = request.data.get("phone", "")
+        first_name = request.data.get("first_name", "")
+        last_name = request.data.get("last_name", "")
+        group = Group.objects.get(pk=request.data.get("group", ""))
+        if not username or not email or not password or not first_name or not last_name or not group:
+            return Response(
+                data={
+                    "message": "fill all required fields."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        new_user = Person.objects.create(
+            email=email, password=password, phone=phone, first_name=first_name, last_name=last_name,
+            username=username
+        )
+        new_user.groups.add(group)
+        return Response(
+            data=PersonSerializer(new_user).data,
             status=status.HTTP_201_CREATED
         )

a660a4a9199adda903b8f10eaefaf61bba84b5ce
Henrique Oliveira <henrique.verrengia@gmail.com>
2019-05-21 22:03:27 -0300

    Implement fixes on current endpoints

--- server/appbackend/api.py
+++ server/appbackend/api.py
@@ -1,14 +1,14 @@
-import coreapi
-import coreschema
+import json
+
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import Group
-from rest_framework import generics, status, permissions, schemas
-from rest_framework.decorators import renderer_classes, api_view, permission_classes
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import generics, status, permissions
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.schemas import ManualSchema
 from rest_framework_jwt.settings import api_settings
-from rest_framework_swagger import renderers
 
 from .serializers import *
 
@@ -16,22 +16,12 @@ jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 
-@api_view(['GET'])
-@renderer_classes([renderers.OpenAPIRenderer, renderers.SwaggerUIRenderer])
-@permission_classes((permissions.AllowAny,))
-def schema_view(request):
-    generator = schemas.SchemaGenerator(title='NossoLar API')
-    return Response(generator.get_schema(request))
-
-
 class StudentApi(generics.ListAPIView):
     serializer_class = StudentSerializer
 
     def get_queryset(self):
-        students = list(Person.objects.filter(groups__name='Students').values())
-        return Response(
-            data=StudentSerializer(students, many=True).data,
-            status=status.HTTP_200_OK)
+        students = list(Person.objects.filter(groups__in=[1]).values())
+        return students
 
 
 class StudentDetailApi(generics.RetrieveAPIView):
@@ -48,6 +38,7 @@ class ClassGroupApi(generics.ListCreateAPIView):
     queryset = ClassGroup.objects.all()
     serializer_class = ClassGroupSerializer
 
+    @swagger_auto_schema(request_body=ClassGroupRequest, responses={200: ClassGroupSerializer})
     def post(self, request, *args, **kwargs):
         title = request.data.get("title", "")
         classroom = request.data.get("classroom", "")
@@ -75,10 +66,15 @@ class ClassGroupDetailApi(generics.RetrieveAPIView):
 
 class StudentClassGroupsApi(generics.ListAPIView):
     serializer_class = ClassGroupSerializer
+    queryset = ClassGroup.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
 
-    def get_queryset(self):
-        student_id = self.kwargs['pk']
+    @swagger_auto_schema(manual_parameters=[openapi.Parameter(
+        name="Authorization", in_=openapi.IN_HEADER,
+        type=openapi.TYPE_STRING, default="Bearer <token>",
+        description="Bearer token acquired on user login")])
+    def get(self, request, *args, **kwargs):
+        student_id = self.queryset.get(pk=kwargs['pk'])
         print("student_id: {}".format(student_id))
         list_of_class_groups = []
         enrollments = list(Enrollment.objects.filter(student_id=self.kwargs['pk']).all())
@@ -86,13 +82,23 @@ class StudentClassGroupsApi(generics.ListAPIView):
         for e in enrollments:
             class_group = ClassGroup.objects.filter(id=e.class_group_id)[:1].get()
             list_of_class_groups.append(class_group)
-        return list_of_class_groups
+        return Response(data=ClassGroupSerializer(list_of_class_groups, many=True).data,
+                        status=status.HTTP_200_OK)
 
 
 class RequestTransferApi(generics.CreateAPIView):
+
+    class RequestTransferRequest(serializers.Serializer):
+        enrollment_id = serializers.IntegerField()
+        target_group_id = serializers.IntegerField()
+
+        class Meta:
+            fields = '__all__'
+
     queryset = TransferRequest.objects.all()
     serializer_class = TransferRequestSerializer
 
+    @swagger_auto_schema(request_body=RequestTransferRequest)
     def post(self, request, *args, **kwargs):
         enrollment_from_id = Enrollment.objects.filter(id=request.data["enrollment_id"])[:1].get()
         target_group_from_id = ClassGroup.objects.filter(id=request.data["target_group_id"])[:1].get()
@@ -109,76 +115,52 @@ class RequestTransferApi(generics.CreateAPIView):
 
 class ConfirmTransferRequestApi(generics.UpdateAPIView):
     queryset = TransferRequest.objects.all()
-    serializer_class = TransferRequestSerializer
+    serializer_class = []
 
-    def put(self, request, *args, **kwargs):
+    def patch(self, request, *args, **kwargs):
         transfer_request = self.queryset.get(pk=kwargs["pk"])
         transfer_request.confirm()
         return Response(TransferRequestSerializer(transfer_request).data)
 
-
-class CreateLessonApi(generics.CreateAPIView):
-    schema = ManualSchema(fields=[coreapi.Field(
-        "id",
-        required=True,
-        description="The class group id for which the lesson will be created",
-        location="path",
-        schema=coreschema.String()
-    )], encoding="application/json")
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
-
-    def post(self, request, *args, **kwargs):
-        class_group = ClassGroup.objects.get(pk=kwargs["pk"])
-        lesson = Lesson.objects.create(
-            class_group=class_group
-        )
-        return Response(
-            data=LessonSerializer(lesson).data,
-            status=status.HTTP_201_CREATED
-        )
+    @swagger_auto_schema(method='put', auto_schema=None)
+    @api_view(['PUT'])
+    def put(self, request, *args, **kwargs):
+        return None
 
 
-class CreateAttendanceApi(generics.CreateAPIView):
+class CreateAttendancesApi(generics.CreateAPIView):
+
     queryset = Attendance.objects.all()
-    serializer_class = AttendanceSerializer
+    serializer_class = RollCallListSerializer
 
     def post(self, request, *args, **kwargs):
         lesson = Lesson.objects.filter(pk=kwargs["pk"])[:1].get()
-        students = list(Person.objects.filter(pk__in=request.data["student_ids"]).all())
+        roll_call = request.data.get("roll_call", "")
         attendances = list()
-        for student in students:
+        for item in roll_call:
+            student = Person.objects.get(pk=item.get("student_id"))
             attendance = Attendance.objects.create(
-                lesson=lesson,
-                student=student
-            )
+                student=student, lesson=lesson, was_present=item.get("was_present"))
             attendances.append(attendance)
-        return Response(
-            data=AttendanceSerializer(attendances, many=True).data,
-            status=status.HTTP_201_CREATED
-        )
+        return Response(data=AttendanceSerializer(attendances, many=True),
+                        status=status.HTTP_201_CREATED)
 
 
 class LoginApi(generics.CreateAPIView):
-    schema = ManualSchema(fields=[
-        coreapi.Field(
-            "email",
-            required=True,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "group",
-            required=True,
-            location="form",
-            schema=coreschema.Integer()
-        )
-    ], encoding='application/json')
+
     permission_classes = []
     serializer_class = PersonSerializer
 
     queryset = Person.objects.all()
 
+    class LoginRequest(serializers.Serializer):
+        email = serializers.CharField()
+        group = serializers.IntegerField()
+
+        class Meta:
+            fields = '__all__'
+
+    @swagger_auto_schema(request_body=LoginRequest)
     def post(self, request, *args, **kwargs):
         email = request.data.get("email", "")
         group = request.data.get("group", "")
@@ -200,27 +182,38 @@ class LoginApi(generics.CreateAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class LoginByPhoneApi(generics.CreateAPIView):
-    schema = schema = ManualSchema(fields=[
-        coreapi.Field(
-            "phone",
-            required=True,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "group",
-            required=True,
-            location="form",
-            schema=coreschema.Integer()
+class CreateLessonApi(generics.CreateAPIView):
+
+    queryset = Lesson.objects.all()
+    serializer_class = []
+
+    @swagger_auto_schema(responses={200: LessonSerializer})
+    def post(self, request, *args, **kwargs):
+        class_group = ClassGroup.objects.get(pk=kwargs["pk"])
+        lesson = Lesson.objects.create(
+            class_group=class_group
         )
-    ], encoding='application/json')
+        return Response(
+            data=LessonSerializer(lesson).data,
+            status=status.HTTP_201_CREATED
+        )
+
+
+class LoginByPhoneApi(generics.CreateAPIView):
+
+    class LoginByPhoneRequest(serializers.Serializer):
+        phone = serializers.CharField()
+        group = serializers.IntegerField()
+
+        class Meta:
+            fields = '__all__'
 
     permission_classes = []
     serializer_class = PersonSerializer
 
     queryset = Person.objects.all()
 
+    @swagger_auto_schema(request_body=LoginByPhoneRequest)
     def post(self, request, *args, **kwargs):
         phone = request.data.get("phone", "")
         group = request.data.get("group", "")
@@ -243,24 +236,18 @@ class LoginByPhoneApi(generics.CreateAPIView):
 
 
 class AuthenticateCollaboratorApi(generics.CreateAPIView):
-    schema = ManualSchema(fields=[
-        coreapi.Field(
-            "email",
-            required=True,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "password",
-            required=True,
-            location="form",
-            schema=coreschema.String()
-        )
-    ], encoding='application/json')
+
+    class AuthCollaboratorRequest(serializers.Serializer):
+        email = serializers.CharField()
+        password = serializers.CharField()
+
+        class Meta:
+            fields = '__all__'
 
     serializer_class = PersonSerializer
     queryset = Person.objects.all()
 
+    @swagger_auto_schema(request_body=AuthCollaboratorRequest)
     def post(self, request, *args, **kwargs):
         email = request.data.get("email", "")
         password = request.data.get("password", "")
@@ -280,49 +267,23 @@ class AuthenticateCollaboratorApi(generics.CreateAPIView):
 
 
 class RegisterApi(generics.CreateAPIView):
-    schema = ManualSchema(fields=[
-        coreapi.Field(
-            "email",
-            required=True,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "password",
-            required=False,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "first_name",
-            required=True,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "last_name",
-            required=True,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "phone",
-            required=False,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "group",
-            required=True,
-            location="form",
-            schema=coreschema.Integer()
-        )
-    ], encoding='application/json')
+
+    class RegisterRequest(serializers.Serializer):
+        email = serializers.CharField()
+        password = serializers.CharField()
+        phone = serializers.CharField()
+        first_name = serializers.CharField()
+        last_name = serializers.CharField()
+        group = serializers.IntegerField()
+
+        class Meta:
+            fields = '__all__'
 
     permission_classes = (permissions.AllowAny,)
     serializer_class = PersonSerializer
     queryset = Person.objects.all()
 
+    @swagger_auto_schema(request_body=RegisterRequest)
     def post(self, request, *args, **kwargs):
         email = request.data.get("email", "")
         password = make_password(request.data.get("password", ""))
@@ -330,7 +291,7 @@ class RegisterApi(generics.CreateAPIView):
         first_name = request.data.get("first_name", "")
         last_name = request.data.get("last_name", "")
         group = Group.objects.get(pk=request.data.get("group", ""))
-        if group is 1:
+        if group is Group.objects.get(pk=1):
             password = make_password('student-default-pass')
         if not email or not password or not first_name or not last_name or not group:
             return Response(
@@ -351,49 +312,29 @@ class RegisterApi(generics.CreateAPIView):
 
 
 class RegisterByPhoneApi(generics.CreateAPIView):
-    schema = ManualSchema(fields=[
-        coreapi.Field(
-            "password",
-            required=False,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "first_name",
-            required=True,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "last_name",
-            required=True,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "phone",
-            required=True,
-            location="form",
-            schema=coreschema.String()
-        ),
-        coreapi.Field(
-            "group",
-            required=True,
-            location="form",
-            schema=coreschema.Integer()
-        )
-    ], encoding='application/json')
+
+    class RegisterByPhoneRequest(serializers.Serializer):
+        phone = serializers.CharField()
+        password = serializers.CharField()
+        first_name = serializers.CharField()
+        last_name = serializers.CharField()
+        group = serializers.IntegerField()
+
+        class Meta:
+            fields = '__all__'
+
     permission_classes = (permissions.AllowAny,)
     serializer_class = PersonSerializer
     queryset = Person.objects.all()
 
+    @swagger_auto_schema(request_body=RegisterByPhoneRequest)
     def post(self, request, *args, **kwargs):
         password = make_password(request.data.get("password", ""))
         phone = request.data.get("phone", "")
         first_name = request.data.get("first_name", "")
         last_name = request.data.get("last_name", "")
         group = Group.objects.get(pk=request.data.get("group", ""))
-        if group is 1:
+        if group is Group.objects.get(pk=1):
             password = make_password('student-default-pass')
         if not phone or not password or not first_name or not last_name or not group:
             return Response(

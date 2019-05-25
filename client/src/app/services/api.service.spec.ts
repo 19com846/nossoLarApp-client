7de9afbe35cf79bfe5e8925078fbb9a42cae1bd3
Estêvão Francisco Fáy <eff.fay@gmail.com>
2019-05-24 22:42:08 -0300

    Register by e-mail

--- client/src/app/services/api.service.spec.ts
+++ client/src/app/services/api.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
+<<<<<<< HEAD:client/src/app/services/api.service.spec.ts
 import { APIService } from './api.service';
 
 describe('APIService', () => {
@@ -7,6 +8,15 @@ describe('APIService', () => {
 
   it('should be created', () => {
     const service: APIService = TestBed.get(APIService);
+=======
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: AuthService = TestBed.get(AuthService);
+>>>>>>> Register by e-mail:client/src/app/auth/auth.service.spec.ts
     expect(service).toBeTruthy();
   });
 });

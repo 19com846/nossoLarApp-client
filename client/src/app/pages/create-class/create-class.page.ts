259be2ca908231345e39b4dcfe1fa9bd8bbab90e
joaobenetasso <joaobenetasso@gmail.com>
2019-06-02 01:24:07 -0300

    create class

--- client/src/app/pages/create-class/create-class.page.ts
+++ client/src/app/pages/create-class/create-class.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Course } from '../../interfaces/course';
+import { APIService } from '../../services/api.service';
+import { Router } from '@angular/router';
+import { Headers, Http, RequestOptions  } from '@angular/http';
 
 @Component({
   selector: 'app-create-class',
@@ -8,13 +12,42 @@ import { NavController } from '@ionic/angular';
 })
 export class CreateClassPage implements OnInit {
 
+  title: string;
+  classroom: string;
+  time: string;
+  year: string;
+  teacher_id: number;
   selectedCourse: string='';
 
-  constructor(private navCtrl:NavController) { }
-  pop(){
-    this.navCtrl.pop();
+  public course: Array<Course>;
+
+  constructor(private navCtrl:NavController, private router: Router, private api: APIService) { }
+
+  createClass(){
+    var headers = new Headers();
+    headers.append("Accept", 'application/json');
+    headers.append('Content-Type', 'application/json' );
+    const requestOptions = new RequestOptions({ headers: headers });
+    let body = {
+      "teacher_id": 4,
+      "course_id": 1,
+     "title": "JOAAAAO",
+     "classroom": "oi",
+     "time": "123213",
+     "semester": "s",
+     "year": 1000
+    }
+    this.api.postClassGroup(body,requestOptions);
   }
   ngOnInit() {
+    this.getAllCourses();
   }
 
+  getAllCourses() {
+    this.api.getAllCourses().subscribe((data: Array<Course>)=> {
+      this.course = data;
+      console.log(this.course)
+    });  
+  }
+  
 }

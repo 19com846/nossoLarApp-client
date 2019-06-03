4af41ca2b4e6aa3bcf122cd9d7b0bedd5259696f
Gomes <gg.gomesguilherme@gmail.com>
2019-06-03 18:21:49 -0300

    enrollment

--- client/src/app/pages/attendance/attendance.page.ts
+++ client/src/app/pages/attendance/attendance.page.ts
@@ -1,6 +1,8 @@
+import { Enrollment } from 'src/app/interfaces/enrollment';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { APIService } from 'src/app/services/api.service';
+import { Headers, Http, RequestOptions  } from '@angular/http';
 
 @Component({
   selector: 'app-attendance',
@@ -12,6 +14,8 @@ export class AttendancePage implements OnInit {
   private courseId: Number;
   public turma: any;
   public selectedArray: any = [];
+  enrollment: Array<Enrollment>;
+ public lesson: any;
 
   constructor(private router: Router, 
     private route: ActivatedRoute,
@@ -22,24 +26,60 @@ export class AttendancePage implements OnInit {
 ngOnInit() {
   this.classGroupId = Number(this.route.snapshot.paramMap.get('classId'));
   this.courseId = Number(this.route.snapshot.paramMap.get('courseId'));
-  console.log(this.classGroupId);
-  console.log(this.courseId);
+  this.getEnrollment(this.classGroupId);
+}
+
+getEnrollment(id) {
+  this.api.getClassGroupEnrollment(id).subscribe((data: Array<Enrollment>) => {
+    this.enrollment = data;
+  }); 
 }
 
 selectMember(data) {
-
-  if (data.attendance1 === false) {
+  if(!data.attendance) {
+    this.selectedArray.push(data);
+  } else {
+  if (data.attendance === false) {
      this.selectedArray.push(data);
    } else {
     let names = this.selectedArray.filter(j=> j.id !== data.id);
     this.selectedArray = names;
-
-  }
+   }
+}
  }
 
  finalizar() {
-  console.log(this.selectedArray)
-   //this.navCtrl.pop();
- }
+   //postar lesson
+   var headers = new Headers();
+   headers.append("Accept", 'application/json');
+   headers.append('Content-Type', 'application/json' );
+   const requestOptions = new RequestOptions({ headers: headers });
+   let body;
+  this.api.postClassGroupLesson(this.classGroupId,body,requestOptions).subscribe((data) => {
+   this.lesson =  data;
+    var a = '';
+  
+    for(let i=0; i<this.selectedArray.length;i++){
+      console.log('teste '+this.selectedArray[i].id)
+        if (i+1===this.selectedArray.length) {
+          a = a + '{"student_id": '+this.selectedArray[i].id+',"was_present":'+this.selectedArray[i].attendance+'}';
+        }
+        else {
+          a = a + '{"student_id": '+this.selectedArray[i].id+',"was_present":'+this.selectedArray[i].attendance+'},';
+        }
+    }
+    console.log('a ' +a)
+  body = '{"roll_call": ['+a+']}';
+  console.log('body '+body)
+  var obj = JSON.parse(body);
+  this.api.postLessonRollCall(this.lesson.id,obj,requestOptions).subscribe((data) => {
+    console.log(data);
+   }); 
+  }); 
+  
+  
 
 }
+}
+
+

2f7e865cb9ddf618157be5e648c46360b3973525
joaobenetasso <joaobenetasso@gmail.com>
2019-06-08 22:36:40 -0300

    fix

--- client/src/app/pages/all-students/all-students.page.ts
+++ client/src/app/pages/all-students/all-students.page.ts
@@ -35,13 +35,8 @@ export class AllStudentsPage implements OnInit {
   }
 
   getItems(ev: any) {
-    // Reset items back to all of the items
     this.initializeItems();
-
-    // set val to the value of the searchbar
     const val = ev.target.value;
-
-    // if the value is an empty string don't filter the items
     if (val && val.trim() != '') {
       this.items = this.items.filter((item) => {
         return (item.first_name.toLowerCase().indexOf(val.toLowerCase()) > -1);
